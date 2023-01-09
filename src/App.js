dacbf1288d2fea809631064f358e9411c047dd57
Abhishek-data <abhishekkr1915@gmail.com>
2023-01-09 15:17:19 +0530

    useMemo and useCallback Hooks Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo, useState } from "react";
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
-function App() {
+const App = () => {
+  const [listTitle, setListTitle] = useState("My List");
+  const [isAcending, setIsacending] = useState(true);
+
+  const titleChangeHandler = useCallback(() => {
+    setListTitle("My Title");
+  }, []);
+
+  const setListOrder = useCallback(() => {
+    setIsacending((prev) => !prev);
+  }, []);
+
+  const listItem = useMemo(() => [5, 4, 3, 2, 1, 6, 7], [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <DemoList title={listTitle} order={isAcending} items={listItem} />
+      <Button onClick={titleChangeHandler}>Change List Title</Button>
+      <Button onClick={setListOrder}>
+        {isAcending
+          ? "change to descending order"
+          : "Change to Ascending Order"}
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Demo/DemoList.js
@@ -0,0 +1,20 @@
+import React, { useMemo } from "react";
+import classes from "./DemoList.module.css";
+const DemoList = (props) => {
+  const { items, order } = props;
+  const sortedList = useMemo(() => {
+    return items.sort((a, b) => (props.order ? a - b : b - a));
+  }, [items, order]);
+  return (
+    <div className={classes.list}>
+      <h2>{props.title}</h2>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(DemoList);
